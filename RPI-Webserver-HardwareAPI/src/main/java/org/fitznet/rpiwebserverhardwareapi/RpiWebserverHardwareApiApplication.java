5d1c80820934874ee5137ed31c348a32a9ea6fda
Matthew Fitzgerald <mattlol85@gmail.com>
2023-03-05 22:21:56 -0500

    project init

---
+++ RPI-Webserver-HardwareAPI/src/main/java/org/fitznet/rpiwebserverhardwareapi/RpiWebserverHardwareApiApplication.java
@@ -0,0 +1,13 @@
+package org.fitznet.rpiwebserverhardwareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpiWebserverHardwareApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpiWebserverHardwareApiApplication.class, args);
+    }
+
+}
---
+++ RPI-Webserver-HardwareAPI/src/test/java/org/fitznet/rpiwebserverhardwareapi/RpiWebserverHardwareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fitznet.rpiwebserverhardwareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpiWebserverHardwareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
