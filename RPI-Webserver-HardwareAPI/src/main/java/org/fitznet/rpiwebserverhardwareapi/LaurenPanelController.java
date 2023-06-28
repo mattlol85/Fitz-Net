f2f0ca20dc9764795fda9f489a1d24f8939b3da2
Matthew Fitzgerald <mattlol85@gmail.com>
2023-06-27 23:55:34 -0400

    Changed pin

--- RPI-Webserver-HardwareAPI/src/main/java/org/fitznet/rpiwebserverhardwareapi/LaurenPanelController.java
+++ RPI-Webserver-HardwareAPI/src/main/java/org/fitznet/rpiwebserverhardwareapi/LaurenPanelController.java
@@ -17,13 +17,13 @@ public class LaurenPanelController {
     private final LED greenLed = new LED(26);
     private final LED blueLed = new LED(16);
     private final LED yellowLed = new LED(12);
-    private final MusicalBuzzer buzzer = new MusicalBuzzer(6);
+    private final MusicalBuzzer buzzer = new MusicalBuzzer(12);
 
     @GetMapping("/hungry")
     public String getHungry() {
         try {
             LOGGER.debug("Toggling LED for hungry");
-            redLed.blink(0.5f,0.5f,5,true);
+            redLed.blink(0.5f, 0.5f, 5, true);
             return "I am hungry!";
         } catch (RuntimeIOException e) {
             return "Error toggling LED: " + e.getMessage();
@@ -33,7 +33,7 @@ public class LaurenPanelController {
     @GetMapping("/love")
     public String getLove() {
         try {
-            greenLed.blink(0.5f,0.5f,5,true);
+            greenLed.blink(0.5f, 0.5f, 5, true);
             return "I love you!";
         } catch (RuntimeIOException e) {
             return "Error toggling LED: " + e.getMessage();
@@ -43,7 +43,7 @@ public class LaurenPanelController {
     @GetMapping("/sad")
     public String getSad() {
         try {
-            blueLed.blink(0.5f,0.5f,5,true);
+            blueLed.blink(0.5f, 0.5f, 5, true);
             return "I am sad.";
         } catch (Exception e) {
             return "Error toggling LED: " + e.getMessage();
@@ -53,7 +53,7 @@ public class LaurenPanelController {
     @GetMapping("/happy")
     public String getHappy() {
         try {
-            yellowLed.blink(0.5f,0.5f,5,true);
+            yellowLed.blink(0.5f, 0.5f, 5, true);
             return "I am happy!";
         } catch (RuntimeIOException e) {
             return "Error toggling LED: " + e.getMessage();
@@ -61,9 +61,9 @@ public class LaurenPanelController {
     }
 
     @GetMapping("/buzzertest")
-public String getBuzzerTest() {
+    public String getBuzzerTest() {
         try {
-            buzzer.playTone(2,440);
+            buzzer.playTone(2, 440);
             return "Buzzer test complete!";
         } catch (RuntimeIOException e) {
             return "Error toggling buzzer: " + e.getMessage();
